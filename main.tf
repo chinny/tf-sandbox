ff09ff42092c329c106517be0acfabd3f2639df3
Jeffrey Chin <jeffreychin89@gmail.com>
2022-06-21 14:00:20 -0700

    test

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+terraform {
+    required_version = ">=1.1.9"
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+      }
+    }
+}
+
+resource "aws_instance" "test_instance" {
+    instance_type = "t3.micro"
+    ami = "ami-030d0fd97bec430c1"
+}
